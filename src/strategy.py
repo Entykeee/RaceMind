6bcb8b67242cd803c14715d6e9639dd61b86e435
Ojas Godambe <ojasgodambe04@gmail.com>
2026-06-05 12:18:16 +0530

    Build pit stop simulation prototype

--- src/strategy.py
+++ src/strategy.py
@@ -31,3 +31,28 @@ def estimate_race_time(driver_laps):
     clean_laps = prepare_lap_times(driver_laps)
 
     return clean_laps["LapTimeSeconds"].sum()
+
+def simulate_pit_stop(driver_laps, pit_lap):
+
+    actual_pit_lap = get_pit_lap(driver_laps)
+
+    actual_race_time = estimate_race_time(driver_laps)
+
+    lap_difference = actual_pit_lap - pit_lap
+
+    estimated_gain = lap_difference * 0.15
+
+    simulated_race_time = (
+        actual_race_time - estimated_gain
+    )
+
+    return {
+        "ActualPitLap": actual_pit_lap,
+        "SimulatedPitLap": pit_lap,
+        "ActualRaceTime": round(actual_race_time, 2),
+        "SimulatedRaceTime": round(simulated_race_time, 2),
+        "Difference": round(
+            actual_race_time - simulated_race_time,
+            2
+        )
+    }
